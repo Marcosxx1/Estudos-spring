65111683b5905acd0ac0373e32f0b5aacaa16d04
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-06-18 10:21:43 -0400

    feat: Implemented @Value(), now it'll be possible to get values from application.properties

--- application/src/main/java/com/example/application/rest/FirstRestController.java
+++ application/src/main/java/com/example/application/rest/FirstRestController.java
@@ -1,11 +1,24 @@
 package com.example.application.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 'expomos'  o caminho "/"
 @RestController
 public class FirstRestController {
+
+    // injetams as dependencias coach.name e team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String coachTeam;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + coachTeam;
+    }
 
     @GetMapping("/")
     public String sayHellow() {
