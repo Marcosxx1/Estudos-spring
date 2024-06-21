9ef6efb91ab2f32f72c4970f1c58d18eccd8392b
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-06-20 20:37:24 -0400

    Adicionar métodos de inicialização e destruição com @PostConstruct e @PreDestroy
    Adicionado métodos  e  na classe ExemploBean com uso das anotações:
    - @PostConstruct para inicializar recursos logo após a criação do bean.
    - @PreDestroy para realizar limpezas finais antes da destruição do bean.
      Esses métodos são essenciais para garantir o correto ciclo de vida e a gestão de recursos no contexto da aplicação.

--- application/src/main/java/com/example/application/controller/TecnicoBolicheController.java
+++ application/src/main/java/com/example/application/controller/TecnicoBolicheController.java
@@ -1,7 +1,10 @@
 package com.example.application.controller;
 
 import com.example.application.service.Tecnico;
+import com.example.application.service.TecnicoBoliche;
 import com.example.application.service.TecnicoFutebol;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class TecnicoBolicheController {
 
     // Definir um campo privado para a dependência
-    private final Tecnico tecnico;
+    private final TecnicoBoliche tecnico;
 
     @Autowired
-    public TecnicoBolicheController(@Qualifier("tecnicoBoliche") Tecnico tecnico) {
+    public TecnicoBolicheController(@Qualifier("tecnicoBoliche") TecnicoBoliche tecnico) {
+        System.out.println("No controller: " + getClass().getSimpleName());
         this.tecnico = tecnico;
+    }
+
+    // Definimos nosso método init
+    @PostConstruct
+    public void init() {
+        System.out.println("No método init(): " + getClass().getSimpleName());
+    }
+
+    // Definimos nosso método destroy
+    @PreDestroy
+    public void destroy() {
+        System.out.println("No método destroy(): " + getClass().getSimpleName());
     }
 
     @GetMapping("/treino-boliche")
